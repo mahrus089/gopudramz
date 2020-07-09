d48f356b3e6dc2e1967e7113338c033ac5d9851c
ramabna <66891940+ramabna@users.noreply.github.com>
2020-07-10 03:03:42 +0700

    Update dorpud.php

--- dorpud.php
+++ dorpud.php
@@ -148,7 +148,7 @@ echo "\e[89m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         sleep(10);
         }
         sleep(10);
-        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0607"}');
+        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EATLAH"}');
         $messageboba09 = fetch_value($boba09,'"message":"','"');
         echo "\n".color("green","# Message: ".$messageboba09);
         sleep(3);
